a45ab59962ad98a950ce910220d93b1d5360116f
FOZEU JEAN MARIE <fozeu.jm@gmail.com>
2020-08-22 20:04:06 +0200

    factory method implementation ok!

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import com.kwd.builder.Car;
 import com.kwd.builder.Director;
 import com.kwd.builder.FerrariCarBuilder;
+import com.kwd.factory.Point;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// builder design pattern demo
+		Main.builderPattern();
+		
+		// factory method design pattern demo
+		Main.factoryMethod();
+
+	}
+
+	public static void builderPattern() {
 		FerrariCarBuilder ferraribuilder = new FerrariCarBuilder();
 
 		Director director = new Director(ferraribuilder);
@@ -14,6 +24,12 @@ public class Main {
 		Car car = director.getCar();
 
 		System.out.println(car);
+	}
+
+	public static void factoryMethod() {
+		// use a static method to build the object - factory method
+		Point pt = Point.newPolarPoint(2, 3);
+		System.out.println(pt);
 	}
 
 }
---
+++ src/com/kwd/factory/Point.java
@@ -0,0 +1,25 @@
+package com.kwd.factory;
+
+public class Point {
+	@SuppressWarnings("unused")
+	private double x, y;
+
+	private Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Point newCartesianPoint(double x, double y) {
+		return new Point(x, y);
+	}
+
+	public static Point newPolarPoint(double rho, double teta) {
+		return new Point(rho * Math.cos(teta), rho * Math.sin(teta));
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+}
